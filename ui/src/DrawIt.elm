81864b30801aad15e46da94181efcd4fca551ee9
Patrik Sandahl <kosmoskatten.software@gmail.com>
2016-11-27 22:56:47 +0100

    Added some more skeleton stuff.

--- ui/src/DrawIt.elm
+++ ui/src/DrawIt.elm
@@ -11,6 +11,7 @@ module DrawIt
 {- | -}
 
 import Html exposing (..)
+import Html.Attributes as A
 
 
 type alias Model =
@@ -29,7 +30,7 @@ init =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "hej" ]
+    viewBoard model
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -40,3 +41,17 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
+
+
+viewBoard : Model -> Html Msg
+viewBoard model =
+    table [ A.class "board" ]
+        [ tr []
+            [ td [ A.class "square active" ] []
+            , td [ A.class "square" ] []
+            ]
+        , tr []
+            [ td [ A.class "square" ] []
+            , td [ A.class "square" ] []
+            ]
+        ]
